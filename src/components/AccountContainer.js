9425eddc510ec1a5457c2f6ddd61a09444552672
Clayton Carlson <claytonrc97@gmail.com>
2023-03-02 09:44:44 -0800

    challenge complete

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
+const BankAPI ='http://localhost:8001/transactions'
+
 
 function AccountContainer() {
+const [transactions, setTransactions] = useState([]);
+const [searchTerm, setSearchTerm] =useState('')
+useEffect(() => {
+  fetch(BankAPI)
+  .then(res => res.json())
+  .then(setTransactions);
+}, [])
+
+
+ const addNewTransaction = (newTransaction) => {
+  setTransactions([...transactions, newTransaction])
+ }
+const filteredTransactions = transactions.filter(trans => trans.description.toLowerCase().includes(searchTerm.toLowerCase()))
   return (
     <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <AddTransactionForm addNewTransaction={addNewTransaction}/>
+      <TransactionsList transactions={filteredTransactions}/>
     </div>
   );
 }
